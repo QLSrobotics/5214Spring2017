a7d7e73fdd31227f87cc1d65926d134edc5fab10
William Ng <williamwu2049@outlook.com>
2018-01-16 17:34:41 -0800

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensor_william.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="colorSensor_william", group="Team5214")
+//@Disabled
+public class colorSensor_william extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private ColorSensor colorSensorFront;
+    private ColorSensor colorSensorBack;
+
+    @Override
+    public void runOpMode() {
+
+        colorSensorFront = hardwareMap.get(ColorSensor.class, "CSF");
+        colorSensorBack = hardwareMap.get(ColorSensor.class, "CSB");
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            telemetry.update();
+        }
+    }
+}
