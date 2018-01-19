fdc8278bc41f4cdd0080708fd46f02dc4da3e2ad
William Ng <williamwu2049@outlook.com>
2018-01-18 17:29:56 -0800

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/touchTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/touchTest.java
@@ -29,12 +29,11 @@ public class touchTest extends LinearOpMode {
 
             // send the info back to driver station using telemetry function.
             // if the digital channel returns true it's HIGH and the button is unpressed.
-            if (digitalTouch.getState() == true) {
-                telemetry.addData("Digital Touch", "Is Not Pressed");
-            } else {
-                telemetry.addData("Digital Touch", "Is Pressed");
-            }
 
+            //IMPORTANT: digitalTouch.getState() is false when it is pressed, it is true when it is not pressed
+            if (gamepad2.b && !digitalTouch.getState()) {
+                //lift goes down
+            }
             telemetry.update();
         }
     }
